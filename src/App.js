c00fa952ce17e71f07880361ba48cd65c43e6c99
Maxim <maksstoma@gmail.com>
2022-06-30 21:06:08 +0300

    added axios, loader, useFetching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './styles/App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {PostList} from "./components/PostList";
 import {PostForm} from "./components/PostForm";
 import {PostFilter} from "./components/PostFilter";
 import {MyModal} from "./components/UI/MyModal/MyModal";
 import {MyButton} from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import {postsApi} from "./api/posts-api";
+import {Loader} from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
 
-    //custom hook
+    //custom hooks
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await postsApi.getAll()
+        setPosts(posts)
+    })
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -39,11 +50,23 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
-            <PostList
-                posts={sortedAndSearchedPosts}
-                title={'List of posts #1'}
-                remove={removePost}
-            />
+            {postError &&
+                <h1>{postError}</h1>
+            }
+            {isPostsLoading
+                ? <div style={{
+                    display: "flex",
+                    justifyContent: 'center',
+                    marginTop: '50px'
+                }}>
+                    <Loader/>
+                </div>
+                : <PostList
+                    posts={sortedAndSearchedPosts}
+                    title={'List of posts #1'}
+                    remove={removePost}
+                />
+            }
         </div>
     );
 }
